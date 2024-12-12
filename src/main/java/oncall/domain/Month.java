e821e8bb29d2a8b1eb372cc559a7454b73d6f645
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-12 15:49:17 +0900

    feat(domain) : Month
    법정 공휴일 업데이트

--- src/main/java/oncall/domain/Month.java
+++ src/main/java/oncall/domain/Month.java
@@ -1,6 +1,12 @@
 package oncall.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import oncall.util.FILE_CONSTANT;
+import oncall.view.FileInputView;
 
 public class Month {
     private int monthInt;
@@ -14,6 +20,14 @@ public class Month {
         this.count_day = getCount_day(monthInt);
         this.days = new int[count_day];
         updateDays();
+        checkHollyDays();
+        printDays();
+    }
+
+    private void printDays() {
+        for (int i = 0; i < count_day; i++) {
+            System.out.print(days[i]);
+        }
     }
 
     private int getCount_day(int monthInt) {
@@ -36,9 +50,29 @@ public class Month {
 
     private void updateDays() {
         for (int i = 0; i < count_day; i++, startDayIndex++) {
-            days[i] = (startDayIndex%7);
+            days[i] = (startDayIndex % 7);
         }
     }
 
+    private void checkHollyDays() {
+        String hollydaysString = FileInputView.readHollyDayFileToString();
+        Scanner scanner = new Scanner(hollydaysString);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            int month = Integer.parseInt(line.split(FILE_CONSTANT.MONTH_DELIMITER.toString())[0]);
+            if (monthInt == month) {
+                updateHollyDays(line.split(FILE_CONSTANT.MONTH_DELIMITER.toString())[1]);
+            }
+        }
+    }
 
+    private void updateHollyDays(String hollyDays) {
+        List<String> hollyDayList = Arrays.stream(hollyDays.split(FILE_CONSTANT.DAY_DELIMITER.toString())).toList();
+        for (String day : hollyDayList) {
+            int dayInt = Integer.parseInt(day);
+            if (this.days[dayInt-1] < 5) {    // 그날이 평일이면
+                this.days[dayInt-1] = 7;  // 법정 공휴일 7로 지정
+            }
+        }
+    }
 }
