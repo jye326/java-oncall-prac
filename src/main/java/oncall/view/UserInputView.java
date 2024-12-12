18ff2750b4af91568f7c45297911301de8a81df6
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-12 15:08:20 +0900

    feat(view) : UserInputView
    - 당번 리스트 입력 받기 기능
     - 중복 에러

--- src/main/java/oncall/view/UserInputView.java
+++ src/main/java/oncall/view/UserInputView.java
@@ -1,11 +1,19 @@
 package oncall.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import oncall.util.ERROR_MESSAGE;
 import oncall.util.REGEX_PATTERN;
+import org.assertj.core.util.Sets;
 
 
 public class UserInputView {
@@ -26,6 +34,47 @@ public class UserInputView {
         }
         return month_StartDay;
     }
+
+    public static List<Deque<String>> readCrewList() {
+        List<Deque<String>> crewList = new ArrayList<>();
+        while (true) {
+            try {
+                System.out.println("평일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+                crewList.add(validateDuplicatedCrewName(validateCrewList(Console.readLine())));
+                System.out.println("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+                crewList.add(validateDuplicatedCrewName(validateCrewList(Console.readLine())));
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return crewList;
+    }
+
+
+    // ret에 중복이 있는지 체크, 없으면 deque로 반환
+    private static Deque<String> validateDuplicatedCrewName(String ret) {
+        ArrayList<String> crewList = (ArrayList<String>) Arrays.stream(ret.split(",")).toList();
+        Set<String> crewSet = new HashSet<>(crewList);
+        if (crewSet.size() != crewList.size()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE.INPUT_FORMAT_ERROR.toString());
+        }
+        Deque<String> deque = new LinkedList<>();
+        for (String crew : crewList) {
+            deque.addFirst(crew);
+        }
+        return deque;
+    }
+
+    private static String validateCrewList(String s) {
+        Pattern pattern = Pattern.compile(REGEX_PATTERN.NAME_LIST_PATTERN.getRegexPattern());
+        Matcher matcher = pattern.matcher(s);
+        if (matcher.matches()) {
+            return s;
+        }
+        throw new IllegalArgumentException(ERROR_MESSAGE.INPUT_FORMAT_ERROR.toString());
+    }
+
 
     // 정규식 사용한 입력 검증
     private static String validateMonth_StartDayPattern(String inputString) {
