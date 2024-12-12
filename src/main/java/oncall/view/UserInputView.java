23c6e425b532d281e712800e7bddcf4b4587c571
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-12 17:45:58 +0900

    refactor(view) : UserInputView
    - 매직 넘버 상수화

--- src/main/java/oncall/view/UserInputView.java
+++ src/main/java/oncall/view/UserInputView.java
@@ -11,7 +11,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import oncall.util.DAY_CONSTANT;
 import oncall.util.ERROR_MESSAGE;
+import oncall.util.FILE_CONSTANT;
+import oncall.util.NUMBER_CONSTANT;
+import oncall.util.OUTPUT_MESSAGE;
 import oncall.util.REGEX_PATTERN;
 import org.assertj.core.util.Sets;
 
@@ -20,13 +24,14 @@ public class UserInputView {
 
     // [0] -> month값, [1] -> 시작 요일 인덱스
     public static int[] readMonth_StartDay() {
-        int[] month_StartDay = new int[2];
+        int[] month_StartDay = new int[NUMBER_CONSTANT.TWO.getInt()];
         String ret;
         while (true) {
             try {
+                System.out.println(OUTPUT_MESSAGE.ASK_MONTH_START_DAY);
                 ret = validateMonth_StartDayPattern(Console.readLine());
-                month_StartDay[0] = validateMonth(ret);
-                month_StartDay[1] = validateStartDay(ret);
+                month_StartDay[NUMBER_CONSTANT.ZERO.getInt()] = validateMonth(ret);
+                month_StartDay[NUMBER_CONSTANT.ONE.getInt()] = validateStartDay(ret);
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -39,9 +44,9 @@ public class UserInputView {
         List<Deque<String>> crewList = new ArrayList<>();
         while (true) {
             try {
-                System.out.println("평일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+                System.out.println(OUTPUT_MESSAGE.ASK_WEEKDAY_CREW_LIST);
                 crewList.add(validateDuplicatedCrewName(validateCrewList(Console.readLine())));
-                System.out.println("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+                System.out.println(OUTPUT_MESSAGE.ASK_HOLLY_DAY_CREW_LIST);
                 crewList.add(validateDuplicatedCrewName(validateCrewList(Console.readLine())));
                 break;
             } catch (IllegalArgumentException e) {
@@ -54,14 +59,14 @@ public class UserInputView {
 
     // ret에 중복이 있는지 체크, 없으면 deque로 반환
     private static Deque<String> validateDuplicatedCrewName(String ret) {
-        ArrayList<String> crewList = (ArrayList<String>) Arrays.stream(ret.split(",")).toList();
+        List<String> crewList = List.of(ret.split(FILE_CONSTANT.DAY_DELIMITER.toString()));
         Set<String> crewSet = new HashSet<>(crewList);
         if (crewSet.size() != crewList.size()) {
             throw new IllegalArgumentException(ERROR_MESSAGE.INPUT_FORMAT_ERROR.toString());
         }
         Deque<String> deque = new LinkedList<>();
         for (String crew : crewList) {
-            deque.addFirst(crew);
+            deque.addLast(crew);
         }
         return deque;
     }
@@ -87,28 +92,28 @@ public class UserInputView {
     }
 
     private static int validateMonth(String inputString) {
-        int month = Integer.parseInt(inputString.split(",")[0]);
+        int month = Integer.parseInt(inputString.split(FILE_CONSTANT.DAY_DELIMITER.toString())[NUMBER_CONSTANT.ZERO.getInt()]);
         if (1 <= month && month <= 12) {
             return month;
         }
-        System.out.println("여기2");
+
         throw new IllegalArgumentException(ERROR_MESSAGE.INPUT_FORMAT_ERROR.toString());
     }
+
     // 월 ~일 중에 하나인지
     private static int validateStartDay(String inputString) {
-        String startDayString = inputString.split(",")[1];
-        HashMap<String,Integer> dayIndexMap = new HashMap<>();
-        dayIndexMap.put("월", 0);
-        dayIndexMap.put("화", 1);
-        dayIndexMap.put("수", 2);
-        dayIndexMap.put("목", 3);
-        dayIndexMap.put("금", 4);
-        dayIndexMap.put("토", 5);
-        dayIndexMap.put("일", 6);
-        if ("월화수목금토일".contains(startDayString)) {
+        String startDayString = inputString.split(FILE_CONSTANT.DAY_DELIMITER.toString())[NUMBER_CONSTANT.ONE.getInt()];
+        HashMap<String, Integer> dayIndexMap = new HashMap<>();
+        dayIndexMap.put(DAY_CONSTANT.MONDAY.getDay(), NUMBER_CONSTANT.ZERO.getInt());
+        dayIndexMap.put(DAY_CONSTANT.TUESDAY.getDay(), NUMBER_CONSTANT.ONE.getInt());
+        dayIndexMap.put(DAY_CONSTANT.WEDNESDAY.getDay(), NUMBER_CONSTANT.TWO.getInt());
+        dayIndexMap.put(DAY_CONSTANT.THURSDAY.getDay(), NUMBER_CONSTANT.THREE.getInt());
+        dayIndexMap.put(DAY_CONSTANT.FRIDAY.getDay(), NUMBER_CONSTANT.FOUR.getInt());
+        dayIndexMap.put(DAY_CONSTANT.SATURDAY.getDay(), NUMBER_CONSTANT.FIVE.getInt());
+        dayIndexMap.put(DAY_CONSTANT.SUNDAY.getDay(), NUMBER_CONSTANT.SIX.getInt());
+        if (DAY_CONSTANT.EVERYDAY.getDay().contains(startDayString)) {
             return dayIndexMap.get(startDayString);
         }
-        System.out.println("여기3");
         throw new IllegalArgumentException(ERROR_MESSAGE.INPUT_FORMAT_ERROR.toString());
     }
 }
